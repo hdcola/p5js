5447f8e759ff03c36c08c5059d2a0f280e4f75e6
hdcola <huangdong@gmail.com>
2021-01-27 22:13:41 -0500

    加入 11.input

---
+++ 11.input/sketch1.js
@@ -0,0 +1,21 @@
+let flowerX = 150;
+let flowerY = 0;
+let Xspeed = 10;
+let Yspeed = 10;
+
+function setup() {
+  createCanvas(400, 400);
+  background(0, 200, 0);
+}
+
+function draw() {
+  fill(255, 128, 0);
+  if (mouseIsPressed) {
+    circle(mouseX, mouseY, 20);
+  }
+}
+
+function mouseClicked(){
+  fill(255,0,0);
+  circle(mouseX,mouseY,40)
+}
